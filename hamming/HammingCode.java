8a9fd6a8f346dfe30b4d97aa2c223745785a6e89
Кирилл Попов <popov.chiks123@yandex.ru>
2024-10-05 16:03:23 +0300

    Commit 2

--- hamming/HammingCode.java
+++ hamming/HammingCode.java
@@ -2,6 +2,8 @@ package hamming;
 public class HammingCode {
     private final ErrorDetection errorDetector;
 
+    private static final String[] BIT_LABELS = {"r1", "r2", "i1", "r3", "i2", "i3", "i4"};
+
     public HammingCode(ErrorDetection errorDetector) {
         this.errorDetector = errorDetector;
     }
@@ -16,16 +18,22 @@ public class HammingCode {
         int errorPosition = errorDetector.detectError(codeword);
 
         if (errorPosition != 0) {
-            System.out.println("Ошибка обнаружена в бите:      " + errorPosition);
+            String bitLabel = getBitLabel(errorPosition);
+            System.out.println("\u001B[31mОшибка обнаружена в бите:      " + errorPosition + " (" + bitLabel + ")\u001B[0m");
             codeword[errorPosition - 1] ^= 1;
             System.out.println("Исправленное кодовое слово:    " + BitUtils.bitArrayToStringWithHighlight(codeword, errorPosition));
         } else {
-            System.out.println("Ошибок не обнаружено.");
+            System.out.println("\u001B[32mОшибок не обнаружено.\u001B[0m");
         }
 
         System.out.println("Статус:                        " + errorDetector.getStatus().getMessage());
 
         return extractDataBits(codeword);
+    }
+
+    private String getBitLabel(int position) {
+        // Индексы идут с 1 до 7, а массив с 0, поэтому нужно уменьшить индекс на 1
+        return BIT_LABELS[position - 1];
     }
 
     private int[] extractDataBits(int[] codeword) {
